3d0568391ad0f2331c5aba7f2dd538a964aab671
Ej Mahadi <ejmahadiair@gmail.com>
2022-02-05 20:37:56 +0600

    file path solved

--- script.js
+++ script.js
@@ -8,14 +8,19 @@
 let nav = document.getElementById("ejnav");
 let lastScrollY = window.scrollY;
 window.addEventListener("scroll", () => {
-    if (lastScrollY > window.scrollY) {
+    if (window.scrollY <= 5) {
+        nav.classList.remove("nav-hidden");
+        nav.classList.remove("nav-back-color");
+    } else if (lastScrollY > window.scrollY) {
         nav.classList.add("nav-hidden");
         nav.classList.add("nav-back-color");
-    } else if (window.scrollY >= 5 || lastScrollY >= 5) {
-        nav.style.background = "transparent";
     } else {
         nav.classList.remove("nav-hidden");
         nav.classList.remove("nav-back-color");
     }
     lastScrollY = window.scrollY;
+    console.log(window.scrollY);
 });
+// if (window.scrollY <= 5) {
+//     nav.style.background = "#000";
+// }
